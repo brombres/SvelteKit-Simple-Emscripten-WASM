6a6a29f6c5bc41c384fa13eb9ceab11544289ad6
Brom Bresenham <brom.bresenham@runegate.com>
2023-09-21 14:32:57 -0400

    [Formatting]

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
   import create_fn from "./Hello";
 
   let message:string = "";
   let sum:number = 0;
 
-	onMount(async () => {
+  onMount(async () => {
     var mod:HelloModule = await create_fn();
     message = mod.hello();
     sum = mod.add(3,4);
-	});
+  });
 </script>
 
 <h1>Simple Example of Emscripten+WASM in SvelteKit</h1>
